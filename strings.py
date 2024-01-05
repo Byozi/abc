f2a50fe0a2afe0fc8d95b231f2f019357b2c56ba
Halil K <berkvero97@gmail.com>
2024-01-05 21:45:41 +0300

    ..
    .

---
+++ strings.py
@@ -0,0 +1,54 @@
+
+START_MSG = """
+Selam <b>{}</b> ! Benim Adım <b>Liva</b>.
+Bir oyun başlatmak istersen 
+ <code>/play</code> butonuna bas!
+"""
+
+ME_MSG = """
+<b>İsim :</b> <code>{}</code>
+<b>Kullanıcı Adı :</b> <code>{}</code>
+<b>Kullanıcı Kimliği :</b> <code>{}</code>
+<b>Dil :</b> <code>{}</code>
+<b>Çocuk Modu :</b> <code>{}</code>
+<b>Toplam Tahmin :</b> <code>{}</code>
+<b>Doğru Tahmin :</b> <code>{}</code>
+<b>Yanlış Tahmin :</b> <code>{}</code>
+<b>Tamamlanmamış Tahmin :</b> <code>{}</code>
+<b>Toplam Soru :</b> <code>{}</code>
+"""
+
+GITHUB_URL = "https://t.me/scrable"
+DEV_URL = "https://t.me/scrable"
+
+AKI_LANG_CODE = {
+    'en': 'English',
+    'ar': 'Arabic',
+    'cn': 'Chinese',
+    'de': 'German',
+    'es': 'Spanish',
+    'fr': 'French',
+    'il': 'Hebrew',
+    'it': 'Italian',
+    'jp': 'Japanese',
+    'kr': 'Korean',
+    'nl': 'Dutch',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'ru': 'Russian',
+    'tr': 'Türkçe',
+    'id': 'Indonesian',
+}
+
+AKI_LANG_MSG = """
+Değiştirilen Dil.
+<b>NOT : Bu, Bot dilini değiştirmez.</b>
+<b>Mevcut Dil :</b> <pre>{}</pre>
+"""
+
+CHILDMODE_MSG = """
+Eğer Çocuk Modu etkinse, akinator NSFW içeriği göstermeyecek.
+<b>Mevcut Durum :</b> <pre>Çocuk modu {} !</pre>
+"""
+
+AKI_FIRST_QUESTION = "Bu soru ilkidir. Daha önceye geri dönemezsiniz!"
